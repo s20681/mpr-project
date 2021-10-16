efc41ed7042aca492b201c1d5dbba5a39d1c8eec
s20681 <s20681@pjwstk.edu.pl>
2021-10-16 10:04:04 +0200

    initial commit

---
+++ src/main/java/com/pjatk/carcreator/CarCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.pjatk.carcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjatk/carcreator/CarCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjatk.carcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
